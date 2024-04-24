06123f2fb67a4ba46d241ec3be6d5fb78deee13d
Nuri Jung <jnooree@snu.ac.kr>
2024-04-24 13:34:44 +0900

    fix: update scripts to quote arguments

--- run_unifold.sh
+++ run_unifold.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-fasta_path=$1
-output_dir_base=$2
-database_dir=$3
-max_template_date=$4
-model_name=$5
-param_path=$6
+fasta_path="$1"
+output_dir_base="$2"
+database_dir="$3"
+max_template_date="$4"
+model_name="$5"
+param_path="$6"
 
 echo "Starting homogeneous searching..."
 unifold-homo-search \
-    --fasta_path=$fasta_path \
-    --max_template_date=$max_template_date \
-    --output_dir=$output_dir_base  \
-    --uniref90_database_path=$database_dir/uniref90/uniref90.fasta \
-    --mgnify_database_path=$database_dir/mgnify/mgy_clusters_2018_12.fa \
-    --bfd_database_path=$database_dir/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
-    --uniclust30_database_path=$database_dir/uniclust30/uniclust30_2018_08/uniclust30_2018_08 \
-    --uniprot_database_path=$database_dir/uniprot/uniprot.fasta \
-    --pdb_seqres_database_path=$database_dir/pdb_seqres/pdb_seqres.txt \
-    --template_mmcif_dir=$database_dir/pdb_mmcif/mmcif_files \
-    --obsolete_pdbs_path=$database_dir/pdb_mmcif/obsolete.dat \
+    --fasta_path="$fasta_path" \
+    --max_template_date="$max_template_date" \
+    --output_dir="$output_dir_base"  \
+    --uniref90_database_path="$database_dir"/uniref90/uniref90.fasta \
+    --mgnify_database_path="$database_dir"/mgnify/mgy_clusters_2018_12.fa \
+    --bfd_database_path="$database_dir"/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
+    --uniclust30_database_path="$database_dir"/uniclust30/uniclust30_2018_08/uniclust30_2018_08 \
+    --uniprot_database_path="$database_dir"/uniprot/uniprot.fasta \
+    --pdb_seqres_database_path="$database_dir"/pdb_seqres/pdb_seqres.txt \
+    --template_mmcif_dir="$database_dir"/pdb_mmcif/mmcif_files \
+    --obsolete_pdbs_path="$database_dir"/pdb_mmcif/obsolete.dat \
     --use_precomputed_msas=True
 
 echo "Starting prediction..."
-fasta_file=$(basename $fasta_path)
-target_name=${fasta_file%.fa*}
+fasta_file="$(basename "$fasta_path")"
+target_name="${fasta_file%.fa*}"
 unifold-inference \
-	--model_name=$model_name \
-	--param_path=$param_path \
-	--data_dir=$output_dir_base \
-	--target_name=$target_name \
-	--output_dir=$output_dir_base
+	--model_name="$model_name" \
+	--param_path="$param_path" \
+	--data_dir="$output_dir_base" \
+	--target_name="$target_name" \
+	--output_dir="$output_dir_base"
--- run_unifold_musse.sh
+++ run_unifold_musse.sh
@@ -1,31 +1,31 @@
 #!/bin/bash -e
 
-fasta_path=$1
-output_dir_base=$2
-param_path=$3
-lm_param_path=$4
+fasta_path="$1"
+output_dir_base="$2"
+param_path="$3"
+lm_param_path="$4"
 
 echo "Starting generating reprentations from the further pre-trained protein language model"
 infer_lm.py \
-    --input $fasta_path \
-    --path $lm_param_path \
-    --output_dir $output_dir_base \
+    --input "$fasta_path" \
+    --path "$lm_param_path" \
+    --output_dir "$output_dir_base" \
     --repr_layers 36 --include per_tok --toks_per_batch 512 --fp16
     
 echo "Starting preparing features for fasta sequences"
 unifold-pack-feat \
-    --fasta_path=$fasta_path \
-    --output_dir=$output_dir_base
+    --fasta_path="$fasta_path" \
+    --output_dir="$output_dir_base"
 
 echo "Starting prediction..."
-fasta_file=$(basename $fasta_path)
-target_name=${fasta_file%.fa*}
+fasta_file="$(basename "$fasta_path")"
+target_name="${fasta_file%.fa*}"
 unifold-inference \
     --model_name=unifold_musse \
-    --param_path=$param_path \
-    --data_dir=$output_dir_base \
-    --target_name=$target_name \
-    --output_dir=$output_dir_base
+    --param_path="$param_path" \
+    --data_dir="$output_dir_base" \
+    --target_name="$target_name" \
+    --output_dir="$output_dir_base"
     
 echo "done"
     
